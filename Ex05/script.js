f29892e0d5800de2dfcab5dc3ef14f5d3a24de08
guerragabi <gabriela_g_sa@estudante.sesisenai.org.br>
2024-06-15 19:34:51 -0300

    add exercicio 5

---
+++ Ex05/script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputTarefa = document.getElementById('input-lista')
+    const btnAddTarefa = document.getElementById('criar-tarefa')
+    const listaTarefas = document.getElementById('lista')
+    
+    function criarTarefa() {
+        const novaTarefa = inputTarefa.value
+        if (novaTarefa === '') {
+            alert("Digite uma tarefa")
+            return
+        }
+
+        const itemLista = document.createElement('li')
+        itemLista.className = 'item-lista'
+
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+
+        const label = document.createElement('label')
+        label.textContent = novaTarefa
+
+
+        checkbox.addEventListener('change', () => {
+            label.classList.toogle('done', checkbox.checked)
+        })
+
+        itemLista.appendChild(checkbox)
+        itemLista.appendChild(label)
+
+        listaTarefas.appendChild(itemLista)
+        inputTarefa.value = ''
+        inputTarefa.focus()
+    }
+
+    btnAddTarefa.addEventListener('click', criarTarefa)
+
+    inputTarefa.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter'){
+            criarTarefa()
+        }
+    })
+})
